cc7f293e3ae862d8b20d028511ae8f9afbde3e4c
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-03-13 16:42:39 -0400

    set up react application

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return <p>Hello Class</p>
+    /*<HorndBeaset title=""description name img*/ 
+  }
+} 
+
+export default Main; 
