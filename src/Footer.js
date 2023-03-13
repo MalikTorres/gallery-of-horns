e58f8be50cb6f9daee3521ff818e581bf1ee3fc6
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-03-13 18:16:04 -0400

    Added new components

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return <h1>Author: Malik Sadiki-Torres</h1>
+  }
+} 
+
+export default Footer; 
--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Header extends React.Component {
   render(){
-    return <h1>Hello People of 301d97!</h1>
+    return <h1>Gallery of Horns</h1>
   }
 } 
 
